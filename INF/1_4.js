de030750228f519abdd8d9facd307b7803ec458a
Benjamín <144442997+BenjaminMasopust@users.noreply.github.com>
2024-09-23 11:08:33 +0200

    Update 1_4.js

--- INF/1_4.js
+++ INF/1_4.js
@@ -1,18 +1,17 @@
 // Funkce s přijímá jeden parametr r (poloměr kruhu)
 function s(r) {
   // Vypočítáme plochu kruhu pomocí vzorce P = π * r^2
-  let a = 3.14 * r * r;
+  let a = Math.PI * r * r;
 
   // Vypočítáme obvod kruhu pomocí vzorce O = 2 * π * r
-  let c = 2 * 3.14 * r;
+  let c = 2 * Math.PI * r;
 
   // Vypíšeme plochu do konzole
-  console.log("Area: " + a);
+  console.log(`Area: ${a}`);
 
   // Vypíšeme obvod do konzole
-  console.log("Circumference: " + c);
+  console.log(`Circumference: ${c}`);
 }
 
 // Zavoláme funkci s s poloměrem 5
 s(5);
-
